049ad2dd83c1d6c0173b84d9b00fb8d0f7defe3a
purple2m <108536436+purple2m@users.noreply.github.com>
2022-12-23 19:33:15 +0900

    연금 페이지

--- assets/ncapi.js
+++ assets/ncapi.js
@@ -120,7 +120,6 @@ function ncapi(get_url, item_id, enchant_level, target){
 	}
 	
 }
-
   
 function item_icon(item_id, enchant_level){
 	this.token = "eyJraWQiOiI0OGEzNzliNS1mNGIxLTQ2Y2ItYTk4Zi0xOWNmM2VjOTEyYTYiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOiIxRjczMzQyNy05RUQ5LTQ5MjctQTEzRS1DMTJEMDU4ODRDOTAifQ.S8ZmQslV8xMQ_gT2dx7C0OGFV0rWFh-vsk5YHMTx32bVB5uvQSQrdSmbLKIiQycsnyCrw-ASkjeqtjBAzFkpbQBel-g3tQpbg8I8P3psOO_kJizvaJtwOsLQaHiF_zqhgQuDaCRZ40yHI49BWMGUukkx-SQhko0HCYy3uaz2D-ovYrg90E_6g38dz2t7UGdruWpkuTxNCHcjS7OYFEP41C0YjERsFM4o6CTGvKhtNXgs6qf--UbAKsFYHf-UPsQI9FRndhvxvO5pRLbnWDHQLGeWoEDh6Y1j396MGWl3vOCVJhBKaGE4pAUZFoCbV1DfB2Ls3Ns1MPd7u3l9QkMCXg"
@@ -187,58 +186,44 @@ function get_recipe(item, lng, type){
 	var step = '';
 	var find = '';
 	let recipe;
-  
+	find += '<h2 class="trade_category_name">';
 	if(type == 'normal'){
-	  find += "<ul class=\"alc_recipe\">";
-	  if(lng == "jp"){
-		find += "<h1>ふつう錬金</h1>";
-	  } else {
-		find += "<h1>일반 연금</h1>";
-	  }
-	  if (data.normal) {
+		if(lng == "jp"){
+		  find += "ふつう錬金";
+		} else {
+		  find += "일반 연금";
+		}
+		
+		find += "</h2><ul class=\"normal_alc\">";
 		for (var i=0; i < data.normal.length;++i){
-			step = data.normal[i].split(',');
-			recipe = step[2].split('-');
-			if(lng == "jp"){
-				find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.jp+" ("+step[1]+")</li>";
-			} else {
-				find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.ko+" ("+step[1]+")</li>";
-			}
+		  step = data.normal[i].split(',');
+		  recipe = step[2].split('-');
+		  if(lng == "jp"){
+			find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.jp+" ("+step[1]+")</li>";
+		  } else {
+			find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.ko+" ("+step[1]+")</li>";
+		  }
 		}
-	  }else{
+	  } else if(type == 'top'){
 		if(lng == "jp"){
-			find += "<li>見つかりません。</li>";
+		  find += "上級錬金";
 		} else {
-			find += "<li>찾을 수 없습니다.</li>";
+		  find += "상급 연금";
+		}
+		find += "</h2><ul class=\"normal_alc\">";
+		for (var i=0; i < data.top.length;++i){
+		  step = data.top[i].split(',');
+		  recipe = step[2].split('-');
+		  if(lng == "jp"){
+			find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.jp+" ("+step[1]+")</li>";
+		  } else {
+			find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.ko+" ("+step[1]+")</li>";
+		  }
 		}
 	  }
-	} else if(type == 'top'){
-		find += "<ul class=\"alc_recipe\">";
-		if(lng == "jp"){
-		find += "<h1>上級錬金</h1>";
-		} else {
-		find += "<h1>상급 연금</h1>";
-		}
-		if (data.top) {
-			for (var i=0; i < data.top.length;++i){
-				step = data.top[i].split(',');
-				recipe = step[2].split('-');
-				if(lng == "jp"){
-				  find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.jp+" ("+step[1]+")</li>";
-				} else {
-				  find += "<li onclick=\"find_material('"+recipe[0]+"', '"+type+"', '"+lng+"');\"><span>"+recipe[0]+"</span><span>"+recipe[1]+"</span> +"+step[0]+" "+data.ko+" ("+step[1]+")</li>";
-				}
-			  }
-		}else{
-			if(lng == "jp"){
-				find += "<li>見つかりません。</li>";
-			} else {
-				find += "<li>찾을 수 없습니다.</li>";
-			}
-		}
-	}
 	find += "</ul>";
   
-	var my_list2=$(".info").eq(0);
-	my_list2.innerHTML = find;
+	var my_list2= document.getElementsByClassName("info");
+	
+	my_list2[0].innerHTML = find;
 }
